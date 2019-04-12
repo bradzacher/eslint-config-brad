21a3852b23a7cf7cd2eba667fe96547a97db5a6a
Brad Zacher <brad.zacher@gmail.com>
2019-04-12 16:13:24 -0700

    fix: don't publish postinstall patch script

--- scripts/removePrivate.js
+++ scripts/removePrivate.js
@@ -6,5 +6,7 @@ const packageJson = require('../package.json')
 const path = require('path')
 
 delete packageJson.private
+// the postinstall should only be used locally...
+delete packageJson.scripts.postinstall
 
 fs.writeFileSync(path.resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJson, null, 2))
